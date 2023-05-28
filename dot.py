4ddc94c05b84f2678430df00068e2f61dd31719d
Jason Scheffel <contact@jasonscheffel.com>
2023-05-28 13:32:35 -0400

    [chore] make basic argparse
    The basics of argparse and the parser works now. I just need to do
    everything now.
    
    Signed-off-by: Jason Scheffel <contact@jasonscheffel.com>

--- dot.py
+++ dot.py
@@ -23,7 +23,7 @@ import shutil
 import subprocess
 import time
 
-from argparse import Namespace
+import argparse
 
 
 def run(cmd: str) -> dict[str, str | int]:
@@ -71,16 +71,61 @@ def remove_folder(folder_path: str) -> None:
     pass
 
 
-def main(args: Namespace) -> None:
+def main(args: argparse.Namespace) -> None:
     pass
 
 
-def assert_dependencies(args: Namespace) -> None:
+def assert_dependencies(args: argparse.Namespace) -> None:
     if args.no_dependencies_check:
         return
 
 
+class CustomFormatter(
+    argparse.ArgumentDefaultsHelpFormatter,
+    argparse.RawDescriptionHelpFormatter,
+):
+    pass
+
+
+def parse_args() -> argparse.Namespace:
+    epilog = """
+-------------------------------------------------------------------------------
+This program requires, or optionally 'requires' other software.
+
+Such software is listed in the README.md file that accompanies this program;
+additionally, a copy of the said file can be found at:
+<https://git.sr.ht/~jason-scheffel/dot>.
+
+SPDX-FileCopyrightText: 2023 Jason Scheffel <contact@jasonscheffel.com>
+SPDX-License-Identifier: AGPL-3.0-or-later
+
+Copyright (C) 2023 Jason Scheffel
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
+    """
+    parser = argparse.ArgumentParser(
+        description="A tool for managing dotfiles.",
+        epilog=epilog,
+        formatter_class=CustomFormatter,
+    )
+
+    arguments = parser.parse_args()
+
+    return arguments
+
+
 if __name__ == "__main__":
+    arguments = parse_args()
     main(arguments)
 
 #
